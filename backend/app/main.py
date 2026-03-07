c45896526abc89a91b22c915433dfafda7688908
horridharsh07 <aquablue3109@gmail.com>
2026-03-07 10:52:54 +0530

    feat: backend app structure and FastAPI server

---
+++ backend/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app= FastAPI(
+    title="Auth-Starter-Kit-API",
+    description="A starter kit for building authentication APIs with FastAPI.",
+    version="1.0.0",
+)
+@app.get("/")
+def health_check():
+    return {"message": "Auth-Starter-Kit-API is running!"}
